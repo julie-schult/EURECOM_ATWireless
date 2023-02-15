b739a5574457ca371ea7ca08dfa606d74aaf314a
laurafven <laurafv@stud.ntnu.no>
2023-02-15 11:52:27 +0100

    updated problem2 and added figures

--- Problem2/problem2.m
+++ Problem2/problem2.m
@@ -8,19 +8,19 @@ close all
     %  gaussian, product channel, relay case
 % 3. h4 = 1/3(h1+h2+h3), average of three i.i.d compl. gaussian
 
-snr_db= -30:30; %SNR interval
+snr_db= -10:2:30; %SNR interval in dB
 bits = 2^10; %Number of bits = 1024
-iterations = 1000; %
-snr_linear = 10.^(snr_db/10);
+iterations = 1000; %cut-off
+snr_linear = 10.^(snr_db/10); %SNR in "normal" unit
 
 P_out = zeros(1,size(snr_db,2)); %empty matrix of sixe 1 x SNR interval
 
 %Iterate through SNR interval
 for iSNR = 1:size(snr_db,2)
 
-    sigma=1/sqrt(snr_linear(iSNR)); %Sigma for AWGN
+    sigma = 1/sqrt(snr_linear(iSNR)); %Sigma for AWGN
     
-    for i=1:iterations
+    for i = 1:iterations
         TX = round(rand(1,bits))*2-1;
 
         %Channel H consists of Nr = 4 column vectors h1, h2, h3 and h4
@@ -29,7 +29,7 @@ for iSNR = 1:size(snr_db,2)
         h3 = complex(randn(1,bits),randn(1,bits));
         h4 = complex(randn(1,bits),randn(1,bits));
 
-        %%%CASE1 Uncomment each case for plot
+        %%%CASE1 Uncomment each case for each plot
         H = [h1' h2' h3' h4'];
         %%%%%%%
 
@@ -48,12 +48,11 @@ for iSNR = 1:size(snr_db,2)
         P_out(iSNR) = P_out(iSNR) + nr_of_errors(i); %Add errors in P_out matrix
     end
 
-    P_out(iSNR) = P_out(iSNR)./iterations./bits;
+    P_out(iSNR) = P_out(iSNR)./iterations./bits; %Average over iterations and nr of bits
 end
 
 figure(1)
-semilogy(snr_db,P_out);
+semilogy(snr_db,P_out); %P_out in log scale
 title('Probability of deep fade')
 xlabel('SNR [dB]')
-ylabel('Outage Probability')
-
+ylabel('Outage Probability [log]')
